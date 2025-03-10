154626c81498e1379794b1853339c1707c5438fd
Yepthatsit <karolgrzywa@herp.pl>
2025-03-10 14:54:22 +0100

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace main
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("something");
+        }
+    }
+}
