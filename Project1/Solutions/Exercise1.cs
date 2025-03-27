7ade398dceb400eeb9619b91dfc5831ef9ab7ac7
KarolGrzywa <karolgrzywa@herp.pl>
2025-03-27 17:24:14 +0100

    something

--- Project1/Solutions/Exercise1.cs
+++ Project1/Solutions/Exercise1.cs
@@ -10,6 +10,7 @@ namespace Project1.Solutions
     {
         public double MaxTime { get; set; } = 100;
         public double DeltaTime { get; set; } = 0.001;
+        public double DeltaXInDerriveative { get; set; } = 0.001;
         public List<double> TimePoints { get; set; } = new();
         public List<double> XValues { get; set; } = new() {2.58};
         public List<double> VelocityValues { get; set; } = new() { 0 };
@@ -17,7 +18,7 @@ namespace Project1.Solutions
         {
             try
             {
-                TimePoints = Enumerable.Range(0, (int)(100.00/0.001)).Select(x => (double)x / 1000).ToList();
+                TimePoints = Enumerable.Range(0, Convert.ToInt32(100.00/0.001)).Select(x => (double)x / 1000).ToList();
             }
             catch(Exception ex)
             {
@@ -28,11 +29,16 @@ namespace Project1.Solutions
         {
             return -Math.Pow(Math.E,-x*x) -  8* Math.Pow(Math.E, -8*(Math.Pow(x-2,2)));
         }
+        private async Task<double> CalculateAcceleration(double x)
+        {
+            return (await CalculatePotentialValue(x + DeltaXInDerriveative) - await CalculatePotentialValue(x - DeltaXInDerriveative));
+        }
         public async Task CalculateExplicitEulerSolution()
         {
             for(int i = 1; i< TimePoints.Count(); i++)
             {
-                
+                VelocityValues.Add(VelocityValues[i - 1] + DeltaTime * await CalculateAcceleration(XValues[i - 1]));
+                XValues.Add(XValues[i - 1] + DeltaTime * VelocityValues[i - 1]);
             }
             //var a = await CalculatePotentialValue(5);
             //Console.WriteLine(a);
